6ceedf0096dbba4b55f13e352cc78112a613b411
Dorin Lazăr <dorin.lazar@gmail.com>
2021-08-26 13:00:37 +0300

    Untested poorconfig

--- src/ff/poorconfig.cpp
+++ src/ff/poorconfig.cpp
@@ -1,34 +1,59 @@
 #include "poorconfig.h"
 using namespace kl;
 
-static std::pair<PValue, Text> _readMap(const Text& fragment, char split) {
-  auto value = Value::createMap();
-  // Text processingLeftOver = fragment;
-  // std::optional<uint32_t> indentLevel;
-  // while (fragment.size() > 0) {
-  //   auto [currentLine, nextData] = processingLeftOver.splitNextLine();
-  //   if (currentLine.size() > 0) {
-  //     if (!indentLevel.has_value()) {
-  //       indentLevel = currentLine.getIndent();
-  //     }
-  //   }
-  //   processingLeftOver = nextData;
-  // }
+const Text CommentCharacter = "#"_t;
 
-  // bool indent = fragment.skipIndent()
-  // while (nextData.)
-  for (const auto& line: fragment.splitLines()) {
-    auto pos = line.pos(split);
-    if (pos.has_value()) {
-      value->add(line.sublen(0, *pos).trim(), line.subpos((*pos) + 1, line.size()).trim());
-    } else {
-      auto txt = line.trim();
-      if (txt.size() > 0) {
-        value->add(line.trim(), ""_t);
+static std::pair<PValue, Text> _readMap(const Text& fragment, char split, uint32_t minIndent = 0) {
+  auto value = Value::createMap();
+  Text processingLeftOver = fragment;
+  std::optional<uint32_t> indentLevel;
+  while (processingLeftOver.size() > 0) {
+    auto [currentLine, nextData] = processingLeftOver.splitNextLine();
+    if (currentLine.size() > 0 && !currentLine.expectws(CommentCharacter).has_value()) {
+      if (!indentLevel.has_value()) {
+        indentLevel = currentLine.getIndent();
+        if (*indentLevel < minIndent) {
+          throw std::invalid_argument("indentation error");
+        }
+      }
+      auto lineContent = currentLine.skipIndent(*indentLevel);
+      if (!lineContent.has_value()) {
+        return {value, processingLeftOver};
+      }
+      if (!lineContent->contains(split)) { // we stop processing here
+        throw std::invalid_argument("No key - value pair in file");
+      }
+      auto [name, v] = lineContent->splitNextChar(split);
+      auto tname = name.trim();
+      auto tvalue = v.trim();
+      if (tname.size() == 0) {
+        throw std::invalid_argument("Empty key name in configuration file");
+      }
+      if (tvalue.size() == 0) {
+        PValue parsedValue;
+        std::tie(parsedValue, nextData) = _readMap(nextData, split, *indentLevel);
+        value->add(tname, parsedValue);
+      } else {
+        value->add(tname, tvalue);
       }
     }
+    processingLeftOver = nextData;
   }
-  return std::make_pair(value, ""_t);
+
+  // // bool indent = fragment.skipIndent()
+  // // while (nextData.)
+  // for (const auto& line: fragment.splitLines()) {
+  //   auto pos = line.pos(split);
+  //   if (pos.has_value()) {
+  //     value->add(line.sublen(0, *pos).trim(), line.subpos((*pos) + 1, line.size()).trim());
+  //   } else {
+  //     auto txt = line.trim();
+  //     if (txt.size() > 0) {
+  //       value->add(line.trim(), ""_t);
+  //     }
+  //   }
+  // }
+  return {value, processingLeftOver};
 }
 
 PValue PoorConfig::parse(const Text& fragment, char split) { return _readMap(fragment, split).first; }
