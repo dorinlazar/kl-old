14437090a9425ccf1381fb63eb7b5bfd4a180947
Dorin Lazăr <dorin.lazar@gmail.com>
2021-09-10 21:32:13 +0300

    bootstrap rename

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p build
-g++ -g3 -std=c++20 -Isrc/ -Isrc/kl -Isrc/klb src/klb/*cpp src/kl/*cpp src/ff/*cpp -o build/klb.exe
+g++ -g3 -std=c++20 -Isrc/ -Isrc/kl -Isrc/klb src/klb/*cpp src/kl/*cpp src/ff/*cpp -o build/klb.bootstrap && echo "build/klb.bootstrap is now ready to be used"
 
