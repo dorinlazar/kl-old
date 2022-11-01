9637fa9e1d9d9deeb5b492b434a932c9caccde18
Dorin Lazăr <dorin.lazar@gmail.com>
2022-11-01 19:41:41 +0200

    removed bootstrap script

--- bootstrap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mkdir -p build
-g++ -g3 -std=c++20 -Isrc/ -Isrc/kl -Isrc/klb src/klb/*cpp src/kl/*cpp src/ff/*cpp -o build/klb.bootstrap -lssl && echo "build/klb.bootstrap is now ready to be used"
-
